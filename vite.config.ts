eef8dfbe479ecd6df1c45cce2d58bc34707d9655
marcoslafoz <marcos.lafoz96@gmail.com>
2025-02-11 01:17:51 +0100

    .

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { version } from './package.json'
 
-const hash = Date.now();
-
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       output: {
-        entryFileNames: `[name].${version}.${hash}.js`,
-        chunkFileNames: `[name].${version}.${hash}.js`,
-        assetFileNames: `[name].${version}.${hash}.[ext]`
+        entryFileNames: `[name].${version}.js`,
+        chunkFileNames: `[name].${version}.js`,
+        assetFileNames: `[name].${version}.[ext]`
       }
     }
   }
